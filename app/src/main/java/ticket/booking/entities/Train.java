0b10f81f1ce9aea6105fc52c533ae32b5d9b5acf
Anurag Behura <behura960@gmail.com>
2025-11-25 17:40:01 +0530

    Initial commit

---
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -0,0 +1,11 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class Train
+{
+    private String trainId;
+    private String trainNo;
+    private List<List<Integer>> seats;
+    private
+}
